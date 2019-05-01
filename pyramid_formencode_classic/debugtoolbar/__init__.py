cdeb50b4e8ceaeabea058ab0698d33a4312cbc9a
jonathan vanasco <jonathan@2xlp.com>
2019-05-01 14:22:21 -0400

    v0.3.0 changes

--- pyramid_formencode_classic/debugtoolbar/__init__.py
+++ pyramid_formencode_classic/debugtoolbar/__init__.py
@@ -4,7 +4,4 @@ def includeme(config):
     """
     Pyramid API hook
     """
-    config.registry.settings['debugtoolbar.panels'].append(FormencodeClassicDebugPanel)
-
-    if 'mako.directories' not in config.registry.settings:
-        config.registry.settings['mako.directories'] = []
+    config.add_debugtoolbar_panel(FormencodeClassicDebugPanel)
